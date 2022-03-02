5a188f9429cbe6bac95e6db6b2d7f2e5d5117358
luanbrazz <luan_silvacpv@hotmail.com>
2022-03-01 21:33:04 -0300

    switch

---
+++ Condicionais (Javascript + Lógica de programação)/Switch/switch.js
@@ -0,0 +1,30 @@
+let velocity = 80
+
+switch(velocity * 2) {
+    case 100:
+        console.log("Sua velocidade é 100km/s")
+        break
+    case 160:
+        console.log("Sua velocidade é 160km/s")
+        break
+    default:
+        console.log("Velocidade não identificada")
+}
+/* >>Sua velocidade é 160km/s */
+
+
+let velocity = 100
+
+switch(velocity * 2) {
+    case 100:
+        console.log("Sua velocidade é 100km/s")
+        break
+    case 160:
+        console.log("Sua velocidade é 160km/s")
+        break
+    default:
+        console.log("Velocidade não identificada")
+}
+/* >>Velocidade não identificada */
+
+
