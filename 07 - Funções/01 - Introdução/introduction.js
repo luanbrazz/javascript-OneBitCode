8f7ec068ed03e6c2755f33591e6b34272b316c42
luanbrazz <luan_silvacpv@hotmail.com>
2022-03-05 12:04:54 -0300

    introduction to functions

--- 07 - Funções/01 - Introdução/introduction.js
+++ 07 - Funções/01 - Introdução/introduction.js
@@ -5,7 +5,7 @@ function greetPilot() {
 greetPilot()
 
 
-// PARAMETROS
+// 01 - PARAMETROS
 function speedUp (velocity, acceleration) {
     let newVelocity = velocity + acceleration
     console.log ("Nova velocidade: " + newVelocity)
@@ -18,5 +18,65 @@ speedUp(40, 20)     /* >>"Nova velocidade: 60" */
 speedUp(70, 05)     /* >>"Nova velocidade: 75" */
 
 
-//PARÂMETROS COM VALOR PADRÃO
+//02 - PARÂMETROS COM VALOR PADRÃO
+function hiPilot (name, message = "Olá") {
+    console.log(message + ", " + name + "!")
+}
+
+hiPilot("John Mars", "Seja bem-vindo")  /* >>"Seja bem-vindo, John Mars!" */
+
+hiPilot("John Mars", )                  /* >>"Olá, John Mars!" */
+
+
+
+
+
+//03 - ARMADILHAS DOS PARÂMETROS
+function hiPilot (name, message = "Olá") {
+    console.log(message + ", " + name + "!")
+}
+
+hiPilot("Luan Braz", "Bem-vindo")   /* >>"Bem-vindo, Luan Braz!" */
+
+hiPilot()       /* >>"Olá, undefined!" */
+
+
+function speedUp(velocity, unit="km/s", acceleration) {
+    let newVelocity = velocity + acceleration
+    console.log("Nova velocidade: " + newVelocity + unit)
+  }
+  
+  speedUp(50,"mi/s",20)     /* >>"Nova velocidade: 70mi/s" */
+
+  speedUp(50,20)            /* >>"Nova velocidade: NaN20" */
+//
+
+//Parametros em ordem
+function speedUp(velocity, acceleration, unit="km/s") {
+let newVelocity = velocity + acceleration
+console.log("Nova velocidade: " + newVelocity + unit)
+}
+
+speedUp(50,"mi/s",20)     /* >>"Nova velocidade: 70mi/s" */
+speedUp(50,20)            /* >>"Nova velocidade: 70km/s" */
+
+
+
+//04 - RETORNO DA FUNÇÃO (return)
+function speedUp(velocity, acceleration) {
+    let newVelocity = velocity + acceleration
+    console.log("Nova velocidade: " + newVelocity)
+    return newVelocity
+}
+
+let velocity = 80
+let acceleration = 5
+
+console.log(velocity)                           /* >>80 */
+
+velocity = speedUp(velocity, acceleration)      /* >>"Nova velocidade: 85" */
+
+console.log(velocity)                           /* >>85 */
+
+speedUp(100, 20)                                /* >>"Nova velocidade: 120" */
 
